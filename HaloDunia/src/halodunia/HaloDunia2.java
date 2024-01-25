652f462061929e63926ddd431a145a5e53110939
codejiva <96323489+codejiva@users.noreply.github.com>
2024-01-25 23:49:44 +0700

    Pertemuan 1
    Mengenal struktur dasar Java

---
+++ HaloDunia/src/halodunia/HaloDunia2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package halodunia;
+
+/**
+ *
+ * @author codejiva
+ */
+public class HaloDunia2 {
+    
+    public static void main(String[] args) {
+        // Yang akan ditampilkan/dijalankan di aplikasi
+        System.out.println("Halo, Dunia!");
+    }
+    
+}
